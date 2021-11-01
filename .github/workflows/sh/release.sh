f4c00bb8c5a81eb97950b067b86ec078a40261be
Andrey Savinovskikh <andrey.savinovskikh@gmail.com>
2021-11-01 23:29:19 +0700

    новый тег

--- .github/workflows/sh/release.sh
+++ .github/workflows/sh/release.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 
-curTag=$(git tag | sort -r | head -1)
-prevTag=$(git tag | sort -r | head -2 | tail -1)
-author=$(git show "$curTag" --pretty=format:"%an" --no-patch)
-date=$(git show "$curTag" --pretty=format:"%ar" --no-patch)
-changelog=$(git log "$prevTag".. --pretty=format:"%h - %s (%an, %ar)\n" | tr -s "\n" " ")
-
-export curTag
-export prevTag
-export author
-export date
-export changelog
+export curTag=$(git tag | sort -r | head -1)
+export prevTag=$(git tag | sort -r | head -2 | tail -1)
+export author=$(git show "$curTag" --pretty=format:"%an" --no-patch)
+export date=$(git show "$curTag" --pretty=format:"%ar" --no-patch)
+export changelog=$(git log "$prevTag".. --pretty=format:"%h - %s (%an, %ar)\n" | tr -s "\n" " ")
 
 searchResult=$(bash ./.github/workflows/sh/searchTask.sh)
 taskId=$(echo "$searchResult" | awk -F '"id":"' '{print $2;exit;}' | awk -F '","' '{print $1;exit;}')
@@ -23,7 +17,14 @@ else
   resultCode=$(bash ./.github/workflows/sh/updateTask.sh)
 fi
 
-echo "Result:"
-echo "${resultCode}"
+codeFirstNum=$(echo "${resultCode}" | cut -c 1)
 
-echo "Release Completed!"
+if [ "$codeFirstNum" = "2" ]
+then
+  echo "Release Completed!"
+  exit 0
+else
+  echo "Error"
+  exit 1
+fi
+
