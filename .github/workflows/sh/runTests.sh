7dfc8cf5c4c3431df41b12a895b43af3453aa64b
Andrey Savinovskikh <andrey.savinovskikh@gmail.com>
2021-11-02 02:08:16 +0700

    Обновлено форматирование тестов

--- .github/workflows/sh/runTests.sh
+++ .github/workflows/sh/runTests.sh
@@ -6,7 +6,7 @@ date=$(git show "$curTag" -s --format=%cd --date=format:'%Y-%m-%d %H:%M:%S' --no
 
 npm ci
 npm run build
-results=$(npm run test 2>&1 | tail -n 3 | tr -s "\n" " ")
+results=$(npm run test 2>&1 | tail -n 4 | tr -s "\n" "\\n")
 
 echo "Тесты проведены"
 
